e4454794f7cc1d0ecc9e2082456eb352adbec4e7
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-05-04 12:13:29 -0400

    Implementation of the InsertCounter class using composition.

--- counter.py
+++ counter.py
@@ -2,27 +2,42 @@ from linked import LinkedList
 from itertools import tee
 
 
-class InsertCounter(LinkedList):
+class InsertCounter:
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, linked_list):
+        self.linked_list = linked_list
         self._counter = 0
 
     def insert(self, value):
-        super().insert(value)
+        self.linked_list.insert(value)
         self._counter += 1
 
     def insert_all(self, iterable):
         iter1, iter2 = tee(iterable)
-        super().insert_all(iter1)
+        self.linked_list.insert_all(iter1)
         self._counter += sum(1 for _ in iter2)
 
     @property
     def counter(self):
         return self._counter
-	
 
-lst = InsertCounter()
+    def remove(self):
+        return self.linked_list.remove()
+
+    def clear(self):
+        self.linked_list.clear()
+
+    def __iter__(self):
+        return self.linked_list.__iter__()
+
+    def __str__(self):
+        return self.linked_list.__repr__()
+
+    def __len__(self):
+        return self.linked_list.__len__()
+
+
+lst = InsertCounter(LinkedList())
 lst.insert(4)
 lst.insert_all([8, 15, 16, 23, 42])
 lst.remove()
